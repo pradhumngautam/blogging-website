ac7ddfb6325f6cb61cabc3c96718b061f89d026e
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-03-10 23:25:42 +0530

    refac: code

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -2,28 +2,31 @@ import { Appbar } from "../components/Appbar";
 import { FullBlog } from "../components/Fullblog";
 import { Spinner } from "../components/Spinner";
 import { useBlog } from "../hooks";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 // atomFamilies/selectorFamilies
 export const Blog = () => {
-    const { id } = useParams();
-    const {loading, blog} = useBlog({
-        id: id || ""
-    });
+  const { id } = useParams();
+  const { loading, blog } = useBlog({
+    id: id || "",
+  });
 
-    if (loading || !blog) {
-        return <div>
-            <Appbar />
-        
-            <div className="h-screen flex flex-col justify-center">
-                
-                <div className="flex justify-center">
-                    <Spinner />
-                </div>
-            </div>
+  if (loading || !blog) {
+    return (
+      <div>
+        <Appbar />
+
+        <div className="h-screen flex flex-col justify-center">
+          <div className="flex justify-center">
+            <Spinner />
+          </div>
         </div>
-    }
-    return <div>
-        <FullBlog blog={blog} />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <FullBlog blog={blog} />
     </div>
-}
+  );
+};
